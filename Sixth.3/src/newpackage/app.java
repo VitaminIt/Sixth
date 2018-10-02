501fc0db497005846c220745be9e66cce60db29b
VitaminIt <43563628+VitaminIt@users.noreply.github.com>
2018-10-02 21:46:20 +0300

    Add files via upload

---
+++ Sixth.3/src/newpackage/app.java
@@ -0,0 +1,12 @@
+package newpackage;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+exponent e=new exponent();
+e.getDouble();
+e.getFloat();
+	}
+
+}
---
+++ Sixth.3/src/newpackage/exponent.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+public class exponent {
+Double d1;
+Float f1;
+void getFloat() {
+	System.out.printf("\nmin float  %.1e ",f1.MIN_VALUE);
+	System.out.printf("max float  %.1e ",f1.MAX_VALUE);
+}
+void getDouble() {
+	System.out.printf("min double  %.1e ",d1.MIN_VALUE);
+	System.out.printf("max double  %.1e ",d1.MAX_VALUE);
+}
+
+}
