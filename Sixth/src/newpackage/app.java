7eb9975d728b3b51a107f501fba24e1ee02cd6b1
VitaminIt <vitamindell@gmail.com>
2018-10-02 09:39:22 +0300

    6.1

---
+++ Sixth/src/newpackage/app.java
@@ -0,0 +1,15 @@
+package newpackage;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Dog d= new Dog();
+Cat c=new Cat();
+Cow cow=new Cow();
+d.whatUSay();
+c.whatUSay();
+cow.whatUSay();
+	}
+
+}
